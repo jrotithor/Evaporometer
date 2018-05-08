ef5b7a62e5d36db9add6733f9e2830161319338c
Jaydeep Rotithor <rotithoj@flip1.engr.oregonstate.edu>
2018-05-08 11:39:56 -0700

    full Geoviz code

---
+++ Geovisualization/Code/README.md
@@ -0,0 +1,4 @@
+# PHAZE
+
+
+This repository contains all of the code used to create the web map and charts that are part of the geovisualization project for the PHAZE Weather Station.
--- Geovisualization/Code/app.js
+++ Geovisualization/Code/app.js
@@ -1,3 +1,6 @@
+//Author: Jaydeep Rotithor
+//Description: Client file that creates a socket connection to the web map page and sends data through it in real time
+
 var express = require('express');
 var app = express();
 var $ = jQuery = require('jquery');
@@ -6,26 +9,31 @@ var http = require("http");
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
+var spawn = require("child_process").spawn;
+var p = spawn("python", ["py2csv.py"], {detached: true});
+var valid_data;
 
 
 
-
-
-
-server.listen(80)
+//Listens on port 5040 to avoid collisions with existing processes
+server.listen(process.env.PORT || 5040)
 
 app.use(express.static(__dirname ));
 
 
 
-
+	//creates the socket connection and sends the data from the csv file through it
     io.sockets.on('connection', function (socket) {
 
 			
-
+			
 			var data = fs.readFileSync('sample_data.csv', 'utf8');
 			var data_subsets = $.csv.toObjects(data);
-            socket.emit('join',  data_subsets);
+			if(!(data_subsets.length == 0))
+			{
+				valid_data = data_subsets
+			}
+            socket.emit('join',  valid_data);
 
 
 
